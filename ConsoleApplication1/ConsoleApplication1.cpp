fca4fb1f4ad95de37b3d7ee8bc2863674f65c1a5
jackysf110 <63942439+jackysf110@users.noreply.github.com>
2021-08-14 18:03:43 +0800

    x

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
@@ -10,6 +10,9 @@ int main()
     
     int x = 0;
     int y = 9;
+    
+    //
+    int z = 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
